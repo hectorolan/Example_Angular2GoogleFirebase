29d6d21d89dd5360997099cf79a5030a35bbe961
olavelek <olanhector@gmail.com>
2016-11-20 06:28:25 -0800

    include Ad Service to App Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// Imports
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
@@ -7,9 +8,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { LoginRoutingModule } from './login/login-routing.module';
 import { MainModule } from './main/main.module';
 import { AdminModule } from './admin/admin.module';
-
+// Declarations
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+// Providers
+import { AdService } from './services/ad.service';
 
 @NgModule({
   declarations: [
@@ -26,7 +29,9 @@ import { LoginComponent } from './login/login.component';
     MainModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    AdService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
