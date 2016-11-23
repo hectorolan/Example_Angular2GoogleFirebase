829150f6ec8692707beed96ebeb7110b9ccaef16
olavelek <olanhector@gmail.com>
2016-11-23 17:42:10 -0400

    update UI MyAds component

--- src/app/admin/myads/myads.component.ts
+++ src/app/admin/myads/myads.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Ad } from '../../models/ad';
+import { AdService } from '../../services/ad.service';
 
 @Component({
   selector: 'app-myads',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyAdsComponent implements OnInit {
 
-  constructor() { }
+  ads: Ad[];
+  counter: string = '';
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private adService: AdService) { }
 
   ngOnInit() {
+      this.ads = this.adService.dummyAd;
+      this.counter = this.ads.length + '';
   }
 
 }
