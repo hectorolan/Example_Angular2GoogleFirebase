a95dd7620b5aee5bf6451f46305c94270453b41c
olavelek <olanhector@gmail.com>
2016-11-27 13:09:24 -0800

    update CreateAd Component

--- src/app/admin/createad/createad.component.ts
+++ src/app/admin/createad/createad.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { Section } from '../../models/section';
 import { Console } from '../../models/console';
+import { Ad } from '../../models/ad';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-createad',
@@ -12,9 +15,99 @@ export class CreateAdComponent implements OnInit {
   sections = Section.Sections;
   consoles = Console.Consoles;
 
-  constructor() { }
+  createAdForm: FormGroup;
+  ad: Ad = new Ad();
+  user: User = new User();
+  active = true;
+  result = '';
+
+  formErrors = {
+    'name': '',
+    'price': '',
+    'console': '',
+    'section': ''
+  };
+
+  validationMessages = {
+    'name': {
+      'required':   'Name is required.',
+      'minlength':  'Name must be at least 3 characters long.'
+    },
+    'price': {
+      'required':     'Price is required.',
+      'invalidEmail': 'Verify price.'
+    },
+    'console': {
+      'required': 'Console is required.'
+    },
+    'section': {
+      'required': 'Section is required.'
+    }
+  };
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.buildForm();
+  }
+
+  buildForm(): void{
+    this.createAdForm = this.formBuilder.group({
+      'name': [this.ad.name, [
+          Validators.required,
+          Validators.minLength(3)
+      ]],
+      'price': [this.ad.price, [
+          Validators.required
+      ]],
+      'console': [this.ad.console, [
+          Validators.required
+      ]],
+      'section': [this.ad.section, [
+          Validators.required
+      ]],
+      'description': [this.ad.description]
+    });
+
+    this.createAdForm.valueChanges.subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged();
+  }
+
+  onSubmit() {
+    this.ad = this.createAdForm.value;
+    this.result = this.ad.price + 'HELLO';
+  }
+
+  cancelChanges() {
+    this.ad = new Ad();
+    this.buildForm();
+  }
+
+  onValueChanged(data?: any) {
+    this.result = '';
+    if (!this.createAdForm) {
+      return;
+    }
+    const form = this.createAdForm;
+
+    for (const field in this.createAdForm.controls) {
+      if (this.createAdForm.contains(field)) {
+        // clear previous error message (if any)
+        this.formErrors[field] = '';
+        const control = form.get(field);
+
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if (messages[key] !== '') {
+              this.formErrors[field] += messages[key] + ' ';
+              this.result += messages[key];
+            }
+          }
+        }
+      }
+    }
   }
 
   keys(dictionary): Array<string> {
