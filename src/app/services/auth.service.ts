e533d5d6a4d0ae5c010a558982cced03cee1fbf4
olavelek <olanhector@gmail.com>
2016-12-01 07:35:39 -0400

    implement AngularFire Firebase in AuthService

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/delay';
+import { AngularFire, FirebaseListObservable, AuthProviders, AuthMethods, FirebaseAuthState  } from 'angularfire2';
 
 
 @Injectable()
@@ -10,14 +11,39 @@ export class AuthService {
   isLoggedIn: boolean = false;
   redirectUrl: string;
 
-  constructor() { }
+  constructor(private firebase: AngularFire) { }
 
-  login(): Observable<boolean> {
-    return Observable.of(true).delay(1000).do(val => this.isLoggedIn = true);
+  login(): any {
+    return this.firebase.auth.login()
+      .then((success) => {
+        this.isLoggedIn = true;
+        return true;
+      })
+      .catch((fail) => {
+        this.isLoggedIn = false;
+        return false;
+      });
   }
 
   logout(): void {
+    this.firebase.auth.logout();
     this.isLoggedIn = false;
+  }
+
+  getEmail(): string {
+    let auth = this.firebase.auth.getAuth();
+    if (auth) {
+      return auth.auth.email ? auth.auth.email : '';
+    }
+    return '';
+  }
+
+  getdisplayName(): string {
+    let auth = this.firebase.auth.getAuth();
+    if (auth) {
+      return auth.auth.displayName ? auth.auth.displayName : '';
+    }
+    return '';
   }
 
 }
