aefe97d6de31dbdae71624aba95793727f32259c
olavelek <olanhector@gmail.com>
2016-12-02 21:03:16 -0400

    update Admin to use name on Account Route

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -8,10 +8,12 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
+  displayName = 'Account';
 
   constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
+    this.displayName = this.authService.user.name == '' ? 'Account' : this.authService.user.name;
   }
 
   logout() {
