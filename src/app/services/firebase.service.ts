ea5074662b1aafc451f249148f24476bf26252f2
olavelek <olanhector@gmail.com>
2016-12-06 12:45:19 -0400

    create ServiceFirebase

---
+++ src/app/services/firebase.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { User } from '../models/user';
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA7cHIY_mihlnv4xefUsQEGE6xoxuwyn7k',
+  authDomain: 'gamesclasificados.firebaseapp.com',
+  databaseURL: 'https://gamesclasificados.firebaseio.com',
+  storageBucket: 'gamesclasificados.appspot.com',
+  messagingSenderId: '292788060773'
+};
+
+@Injectable()
+export class FirebaseService {
+  user: User = new User();
+  auth: any;
+  database: any;
+  storage: any;
+
+  constructor() {
+    firebase.initializeApp(firebaseConfig);
+    this.auth = firebase.auth();
+    this.database = firebase.database();
+    this.storage = firebase.storage();
+  }
+}
