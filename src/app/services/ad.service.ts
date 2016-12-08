ccec2aa99fc9ff1197d19f9e60fad5d34061d3fa
olavelek <olanhector@gmail.com>
2016-12-08 17:28:56 -0400

    implement AdService using Firebase

--- src/app/services/ad.service.ts
+++ src/app/services/ad.service.ts
@@ -1,25 +1,92 @@
 import { Injectable } from '@angular/core';
 import { Ad } from '../models/ad';
+import { User } from '../models/user';
+import { FirebaseService } from './firebase.service';
 
 @Injectable()
 export class AdService {
 
   dummyAd: Ad[] = Ad.dummyAds;
 
-  constructor() { }
+  constructor(private firebaseService: FirebaseService) { }
 
-  getAds(console: string, section = ''): Ad[] {
+  getAd(key: string): Promise<Ad> {
+    return Promise.resolve(
+      this.firebaseService.database.ref('ads/' + key).once('value').then((snapshot) => {
+        return snapshot.val();
+      })
+    );
+  }
+
+  getAds(console: string, section = ''): Promise<Ad[]> {
     if (console === '') {
-      return [];
+      return Promise.resolve([]);
     }
-    let tempAds: Ad[];
-    if (console !== '') {
-      tempAds = this.dummyAd.filter(ad => ad.console === console);
-    }
-    if (section !== '') {
-      tempAds = tempAds.filter(ad => ad.section === section);
-    }
-    return tempAds;
+    return Promise.resolve(this.getAdsByConsole(console).then((ads) => {
+      if (section !== '') {
+        return this.getAdsBySection(section, ads);
+      }
+      return ads;
+    }));
+  }
+
+  getAdsByConsole(console: string): Promise<Ad[]> {
+    return Promise.resolve(
+      this.firebaseService.database.ref('ads').orderByChild("console").equalTo(console).once('value').then((snapshot) => {
+        let ads: Ad[] = [];
+        snapshot.forEach((childSnapshot) => {
+          let ad: Ad = childSnapshot.val();
+          ad.id = childSnapshot.key;
+          ads.push(ad);
+        });
+        return ads;
+      })
+    );
+  }
+
+  getAdsBySection(section: string, ads: Ad[]): Ad[] {
+    return ads.filter(ad => ad.section === section);
+  }
+
+  getUserAds(user: User): Promise<Ad[]>{
+    return Promise.resolve(
+      this.firebaseService.database.ref('/user-ads/' + user.id + '/').once('value').then((snapshot) => {
+        let ads: Ad[] = [];
+        snapshot.forEach((childSnapshot) => {
+          let ad: Ad = childSnapshot.val();
+          ad.id = childSnapshot.key;
+          ads.push(ad);
+        });
+        return ads;
+      })
+    );
+  }
+
+  saveAd(ad: Ad, user: User): Promise<any>{
+    ad.owner = user.name;
+    ad.email = user.emailOnAd == true ? user.email : '';
+    ad.city = user.city;
+    ad.telephone = user.telephoneOnAd == true ? user.telephone : '';
+    ad.expDate = new Date();
+    ad.expDate.setDate(ad.expDate.getDate() + 21);
+    let contactMethod = '';
+    contactMethod += user.methodCall ? true : "Call;";
+    contactMethod += user.methodTextMessage ? true : "Text;";
+    contactMethod += user.methodEmail ? true : "Email";
+    ad.contactMethod = contactMethod;
+
+    // Get a key for a new Post.
+    var newPostKey = this.firebaseService.database.ref().child('ads').push().key;
+    var updates = {
+      ['/ads/' + newPostKey]: ad,
+      ['/user-ads/' + user.id + '/' + newPostKey]: ad
+    };
+
+    return Promise.resolve(this.firebaseService.database.ref().update(updates));
+  }
+
+  deleteAd(){
+    
   }
 
 }
