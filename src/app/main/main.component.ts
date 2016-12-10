be5638d99717540d5ed9c7e2b2e9a156392defe0
olavelek <olanhector@gmail.com>
2016-12-09 21:52:40 -0400

    set Sidenav and adjust for mobiles

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ElementRef, ViewChild, Renderer} from '@angular/core';
 import { Console } from '../models/console';
 import { AuthService } from '../services/auth.service';
 
@@ -9,10 +9,14 @@ import { AuthService } from '../services/auth.service';
 })
 export class MainComponent implements OnInit {
   consoles = Console.Consoles;
+  @ViewChild('navpanel') navpanel: ElementRef;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private rd: Renderer) { }
 
   ngOnInit() {
+    if (window.innerHeight <= 500) {
+      this.rd.invokeElementMethod(this.navpanel, 'close');
+    }
   }
 
   logout() {
@@ -21,5 +25,14 @@ export class MainComponent implements OnInit {
 
   keys(dictionary): Array<string> {
     return Object.keys(dictionary);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    if(event.target.innerWidth && event.target.innerWidth > 500){
+      this.rd.invokeElementMethod(this.navpanel, 'open');
+    }else{
+      this.rd.invokeElementMethod(this.navpanel, 'close');
+    }
   }
 }
