d6fdc9d5d606575b766da2138661a49072fcb99e
olavelek <olanhector@gmail.com>
2016-12-16 19:41:16 -0400

    rename Variable

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -7,9 +7,9 @@ export class UserService {
 
   constructor(private firebaseService: FirebaseService) { }
 
-  getUser(Userid: string): Promise<User> {
+  getUser(userId: string): Promise<User> {
     return Promise.resolve(
-      this.firebaseService.database.ref('/users/' + Userid).once('value').then(
+      this.firebaseService.database.ref('/users/' + userId).once('value').then(
         function(snapshot) {
           let user: User = snapshot.val();
           return user;
