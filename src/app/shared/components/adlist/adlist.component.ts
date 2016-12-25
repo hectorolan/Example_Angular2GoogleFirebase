a06928b60e8e3432f9bbcdbd51fad208313c1d70
olavelek <olanhector@gmail.com>
2016-12-25 14:46:12 -0400

    update View

--- src/app/shared/components/adlist/adlist.component.ts
+++ src/app/shared/components/adlist/adlist.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Ad } from '../../../models/ad';
+import { Section } from '../../../models/section';
+import { Console } from '../../../models/console';
+import { IgdbService } from '../../../services/igdb.service';
 
 @Component({
   selector: 'app-adlist',
@@ -7,11 +10,14 @@ import { Ad } from '../../../models/ad';
   styleUrls: ['./adlist.component.css']
 })
 export class AdlistComponent implements OnInit {
+  sections = Section.Sections;
+  consoles = Console.Consoles;
 
   @Input()
   ads: Ad[];
 
-  constructor() { }
+  constructor(
+    private igdbService: IgdbService) { }
 
   ngOnInit() {
   }
