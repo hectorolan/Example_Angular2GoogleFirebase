f4ef0c781394193fd3d67984d08dbf00bb8ad35b
olavelek <olanhector@gmail.com>
2017-01-06 20:14:53 -0400

    set Reoute for verify email component

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { EmailVerificationComponent } from './email-verification/email-verification.component';
 import { AuthService } from '../services/auth.service';
 import { AuthGuardService } from '../services/auth-guard.service';
 
@@ -11,8 +12,8 @@ const routes: Routes = [
     component: LoginComponent
   },
   {
-    path: 'signup',
-    component: SignUpComponent
+    path: 'verify-email',
+    component: EmailVerificationComponent
   }
 ];
 
